9eb40debd3fc5231fb0be2b62d4f5511a960babb
jangmii <0310_rose@naver.com>
2019-06-26 00:30:26 +0900

    need_to_download_club_img

--- SKKUUU-master/public/js/addclub.js
+++ SKKUUU-master/public/js/addclub.js
@@ -1,14 +1,5 @@
-const addcardbtn = document.querySelector("#addcardbtn");
-addcardbtn.addEventListener("click",handleClickadd);
-function handleClickadd(){
-    addcardbtn.className="btn btn-secondary";
-}
-
-
-
 const clubRow = document.querySelector(".clubrow"),
-input = document.querySelector(".clubinput"),
-savebtn = input.querySelector("#savebtn");
+savebtn = document.querySelector("#savebtn");
 
 let clubs = [];
 
@@ -19,59 +10,61 @@ const clubObj = {
     id
 }
 clubs.push(clubObj);
+console.log(clubs);
 var div1 = document.createElement("div");
 div1.className="col-12 col-lg-6";
+
 var div2 = document.createElement("div");
 div2.className="single-blog-area mb-100 wow fadeInUp";
-div2.date-wow-delay = "250ms";
+// div2.data-wow-delay = "500ms";
 
-var img = document.creageElement("img");
+var img = document.createElement("img");
 img.src=`../img/theme-img/a${id}.jpg`;
 div2.appendChild(img);
 
-var div4 = document.createElement("div");
-div4.className="blog-content";
+var div3 = document.createElement("div");
+div3.className="blog-content";
 
 var a = document.createElement("a");
-a.className="blog-headline";
 a.href="#";
+a.className="blog-headline";
 
-var h4 = document.createElement("h4");
-h4.appendChild(document.createTextNode(name));
-a.appendChild(h4);
-div4.appendChild(a);
+var h = document.createElement("h4");
+h.appendChild(document.createTextNode(name));
+a.appendChild(h);
+div3.appendChild(a);
 
-var div5 = document.createElement("div");
-div5.className = "meta d-flex align-items-center";
+var div4 = document.createElement("div");
+div4.className = "meta d-flex align-items-center";
 
 var a2 = document.createElement("a");
 a2.appendChild(document.createTextNode("Seoul/Suwon"));
-div5.appendChild(a2);
+div4.appendChild(a2);
 
 var a3 = document.createElement("a");
-a3.appendChild(document.createTextNode("Art &amp; Culture"));
-div5.appendChild(a3);
-div4.appendChild(div5);
+a3.appendChild(document.createTextNode("Art & Culture"));
+div4.appendChild(a3);
+div3.appendChild(div4);
 
 var p = document.createElement("p");
-p.appendChild(document.createTextNode(description));
-div4.appendChild(p);
-
+p.appendChild(document.createTextNode(clubObj.description));
+div3.appendChild(p);
+div2.appendChild(div3);
 div1.appendChild(div2);
 clubRow.appendChild(div1);
 }
 
 function handleClick(){
-const inputClubName = document.getElementById("club-name");
-const inputClubSul = doument.getElementById("club-sul");
+const inputClubName = document.querySelector("#club-name");
+const inputClubSul = document.querySelector("#club-sul");
 createClub(inputClubName.value, inputClubSul.value, clubs.length+1);
 }
 
 function init(){
 
-savebtn.addEventListener("click",handleClick);
 createClub("Hogwarts","성균관대학교 중앙 마술동아리",1);
 createClub("SKKUO","SKKU Orchestra",2);
+savebtn.addEventListener("click",handleClick);
 
 }
 
